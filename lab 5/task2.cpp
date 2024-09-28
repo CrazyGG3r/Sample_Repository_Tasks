a9013f68847a1f5e6afc3851b0eafa79dd4ab582
Shaheer Ul Islam <54592666+CrazyGG3r@users.noreply.github.com>
2024-09-28 19:07:16 +0500

    Add files via upload

---
+++ lab 5/task2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("           \t\t  ___________\n");
+    printf("           \t\t /           \\ \n");
+    printf("           \t        /  /\\_/\\      \\ \n");
+    printf("           \t       /  ( . . )  ^   \\ \n");
+    printf("           \t      /    )   (  ((    \\ \n");
+    printf("\"cat\"\t\t     |    ( > < )_))     |\n");
+    printf("           \t      \\   -----         /\n");
+    printf("           \t       \\   K24-XXXX    /\n");
+    printf("           \t\t\\             /\n");
+    printf("           \t\t \\___________/\n");
+    
+    return 0;
+}
+
